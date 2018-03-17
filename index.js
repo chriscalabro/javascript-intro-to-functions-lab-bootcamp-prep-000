cfb30c8f2e10c920c8b3b2e5e0bb3c989f8f7ec8
Chris Calabro <chriscalabro@gmail.com>
2018-03-17 00:43:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+// TEST 1
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+shout.string("hello")
+
+// TEST 2
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+string.whisper("HELLO")
+
+// TEST 3
+function logShout(string)
+{
+  var string = string.toUpperCase()
+  console.log(string)
+}
+
+logShout("hello")
+
+// TEST 4
+function logWhisper(string)
+{
+  var string = string.toLowerCase()
+  console.log(string)
+}
+logWhisper("HELLO")
+
+// TEST 5 
+function sayHiToGrandma(string)
+{
+    return`I can\'t hear you!`
+}
+sayHiToGrandma("hello!")
+
